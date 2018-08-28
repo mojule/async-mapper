e4c9eb74f44e719f32fa6fb5b5dab0f8c042a4ca
Nik Coughlin <nrkn.com@gmail.com>
2018-08-28 12:24:27 +1200

    Dist

--- dist/test/fs-mapper.d.ts
+++ dist/test/fs-mapper.d.ts
@@ -1,6 +1,6 @@
 import { PredicateMap, FnMap } from '../types';
-declare type FsNode = [string, string, ...any[]];
+export declare type FsNodeId = 'file' | 'directory';
+export declare type FsNode = [FsNodeId, string, ...any[]];
 export declare const predicates: PredicateMap<string>;
 export declare const map: FnMap<string, FsNode>;
-export declare const fsMapper: (value: any, options?: import("../types").MapperOptions<string, [string, string, ...any[]]> | undefined) => any;
-export {};
+export declare const fsMapper: (value: any, options?: import("../types").MapperOptions<string, [FsNodeId, string, ...any[]]> | undefined) => any;
